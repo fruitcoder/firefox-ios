bdc24b8cb3a0244e2bca5dfa7f3d2cc804403b0a
Emily Toop <etoop@mozilla.com>
2015-10-27 17:43:50 +0000

    Bug 1189870 - Find search URLs and extract search terms. * Ensure that all search engines are used in the search for the search terms otherwise if we change search engine we lose the terms. * Create separate delegate method just to get display text

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -870,6 +870,7 @@ extension BrowserViewController {
 }
 
 extension BrowserViewController: URLBarDelegate {
+
     func urlBarDidPressReload(urlBar: URLBarView) {
         tabManager.selectedTab?.reload()
     }
@@ -931,6 +932,10 @@ extension BrowserViewController: URLBarDelegate {
         } else {
             return [copyAddressAction]
         }
+    }
+
+    func urlBarDisplayTextForURL(url: NSURL?) -> String? {
+        return profile.searchEngines.queryForSearchURL(url) ?? url?.absoluteString
     }
 
     func urlBarDidLongPressLocation(urlBar: URLBarView) {
