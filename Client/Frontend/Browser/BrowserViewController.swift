4eb75b80dc1d3203f9ca4a921d6dc20ca805555a
James Hugman <james@hugman.tv>
2015-10-29 20:13:59 +0000

    Bug 1162174 – tightening up the code a little.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1092,72 +1092,72 @@ extension BrowserViewController: BrowserToolbarDelegate {
     }
 
     func browserToolbarDidPressShare(browserToolbar: BrowserToolbarProtocol, button: UIButton) {
-        if let selected = tabManager.selectedTab {
-            if let url = selected.displayURL {
-                let printInfo = UIPrintInfo(dictionary: nil)
-                printInfo.jobName = url.absoluteString
-                printInfo.outputType = .General
-                let renderer = BrowserPrintPageRenderer(browser: selected)
+        if let selectedTab = tabManager.selectedTab, url = selectedTab.displayURL {
+            let printInfo = UIPrintInfo(dictionary: nil)
+            printInfo.jobName = url.absoluteString
+            printInfo.outputType = .General
+            let renderer = BrowserPrintPageRenderer(browser: selectedTab)
 
-                let activityItems = [printInfo, renderer, selected.title ?? url.absoluteString, self]
+            let activityItems = [printInfo, renderer, selectedTab.title ?? url.absoluteString, self]
 
-                let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+            let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
 
-                // Hide 'Add to Reading List' which currently uses Safari.
-                // Also hide our own View Later… after all, you're in the browser!
-                let viewLater = NSBundle.mainBundle().bundleIdentifier! + ".ViewLater"
-                activityViewController.excludedActivityTypes = [
-                    UIActivityTypeAddToReadingList,
-                    viewLater,                        // Doesn't work: rdar://19430419
-                ]
+            // Hide 'Add to Reading List' which currently uses Safari.
+            // Also hide our own View Later… after all, you're in the browser!
+            let viewLater = NSBundle.mainBundle().bundleIdentifier! + ".ViewLater"
+            activityViewController.excludedActivityTypes = [
+                UIActivityTypeAddToReadingList,
+                viewLater,                        // Doesn't work: rdar://19430419
+            ]
 
-                // Add 1Password to share sheet
-                let webView = selected.webView
-                OnePasswordExtension.sharedExtension().createExtensionItemForWebView(webView!, completion: {(extensionItem, error) -> Void in
-                    if extensionItem == nil {
-                        log.error("Failed to create the 1Password extension item: \(error).")
-                        return
-                    }
+            // Add 1Password to share sheet
+            OnePasswordExtension.sharedExtension().createExtensionItemForWebView(selectedTab.webView!, completion: {(extensionItem, error) -> Void in
+                if extensionItem == nil {
+                    log.error("Failed to create the 1Password extension item: \(error).")
+                    return
+                }
 
-                    // Set the 1Password extension item property
-                    self.onePasswordExtensionItem = extensionItem
-                })
+                // Set the 1Password extension item property
+                // This needs to be ready by the time 
+                // activityViewController(activityViewController:, activityType:) is called,
+                // which is after the user taps the button. So a million cycles away.
+                self.onePasswordExtensionItem = extensionItem
+            })
 
-                activityViewController.completionWithItemsHandler = { activityType, completed, returnedItems, activityError in
-                    log.debug("Selected activity type: \(activityType).")
-                    if completed {
-                        if let selectedTab = self.tabManager.selectedTab {
-                            // We don't know what share action the user has chosen so we simply always
-                            // update the toolbar and reader mode bar to refelect the latest status.
-                            self.updateURLBarDisplayURL(selectedTab)
-                            self.updateReaderModeBar()
+            activityViewController.completionWithItemsHandler = { activityType, completed, returnedItems, activityError in
+                log.debug("Selected activity type: \(activityType).")
+                if !completed {
+                    return
+                }
+                // We don't know what share action the user has chosen so we simply always
+                // update the toolbar and reader mode bar to refelect the latest status.
+                self.updateURLBarDisplayURL(selectedTab)
+                self.updateReaderModeBar()
 
-                            if self.isPasswordManagerActivityType(activityType) {
-                                if returnedItems != nil {
-                                    OnePasswordExtension.sharedExtension().fillReturnedItems(returnedItems, intoWebView: selected.webView!, completion: { (success, returnedItemsError) -> Void in
-                                        if success == false {
-                                            log.error("Failed to fill item into webview: \(returnedItemsError).")
-                                        }
-                                    })
-                                }
-                                else {
-                                    // Code for other custom activity types
-                                }
+                if self.isPasswordManagerActivityType(activityType) {
+                    if returnedItems != nil {
+                        OnePasswordExtension.sharedExtension().fillReturnedItems(returnedItems, intoWebView: selectedTab.webView!, completion: { (success, returnedItemsError) -> Void in
+                            if success == false {
+                                log.error("Failed to fill item into webview: \(returnedItemsError).")
                             }
-                        }
+                        })
+                    }
+                    else {
+                        // Code for other custom activity types
                     }
                 }
-
-                if let popoverPresentationController = activityViewController.popoverPresentationController {
-                    // Using the button for the sourceView here results in this not showing on iPads.
-                    popoverPresentationController.sourceView = self.toolbar ?? self.urlBar
-                    popoverPresentationController.sourceRect = button.frame ?? button.frame
-                    popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirection.Up
-                    popoverPresentationController.delegate = self
-                }
-                self.presentViewController(activityViewController, animated: true, completion: nil)
             }
+
+            if let popoverPresentationController = activityViewController.popoverPresentationController {
+                // Using the button for the sourceView here results in this not showing on iPads.
+                popoverPresentationController.sourceView = self.toolbar ?? self.urlBar
+                popoverPresentationController.sourceRect = button.frame ?? button.frame
+                popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirection.Up
+                popoverPresentationController.delegate = self
+            }
+            self.presentViewController(activityViewController, animated: true, completion: nil)
         }
+
     }
 }
 
